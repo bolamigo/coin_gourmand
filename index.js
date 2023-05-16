d3af96b22632571653fa2ffa98ae8d9461802d27
bolamigo <mendynicol@cy-tech.fr>
2023-05-16 21:08:27 +0200

    TODO : index.js
    Signed-off-by: bolamigo <mendynicol@cy-tech.fr>

---
+++ index.js
@@ -0,0 +1,5 @@
+// TODO for each result (SQL), add an element with class "item" and id="<recipe.title>" (SQL, sans les espaces)
+// TODO for each element with class "item" :
+//        onclick -> recipe/<recipe.title sans espaces>.html
+//        image = image/<recipe.title sans espaces>.jpg
+//        title(h2) -> <recipe.title> (SQL)
