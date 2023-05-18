72caa9dd3e532aee672bb14d86a6c0f977de7a5e
bolamigo <mendynicol@cy-tech.fr>
2023-05-18 11:02:03 +0200

    items are clickable
    Signed-off-by: bolamigo <mendynicol@cy-tech.fr>

--- index.js
+++ index.js
@@ -3,3 +3,15 @@
 //        onclick -> recipe/<recipe.title sans espaces>.html
 //        image = image/<recipe.title sans espaces>.jpg
 //        title(h2) -> <recipe.title> (SQL)
+
+// At page load
+$(document).ready(function() {
+    $('.item').click(function() {
+        let id = $(this).attr('id');
+        open_link_new_tab(`recipe/${id}.html`);
+    });
+    $('.item').hover(function() {
+        let id = $(this).attr('id');
+        $(this).prop('title', id);
+    });
+});
