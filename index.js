f1b0ccb270442d8a69777a40f3b399bd8c2e0841
VerbalKint95 <constantin@cy-tech.fr>
2023-05-18 16:05:07 +0200

    comments

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 // TODO for each result (SQL), add an element with class "item" and id="<recipe.title>" (SQL, sans les espaces)
-// TODO for each element with class "item" :
-//          image : image/<id>.jpg
-//          title(h2) : format_fr(<id>)
+
 
 // At page load
 $(document).ready(function() {
     $('.item').each(function () {
+        // each item get clickable class
         $(this).addClass('clickable');
+        // get id of item
         const id = $(this).attr('id');
+        // auto-generate img/title/link with id
         $(this).append(`<img src="image/${id}.jpg">`);
         $(this).append(`<div class='flex-container'><h2>${format_fr(id)}</h2></div>`);
         $(this).prop('title', format_fr(id));
+        // open in new tab
         $(this).click(function(){
             open_link_new_tab(`${window.location.href}recipe/${id}.html`);
         });
