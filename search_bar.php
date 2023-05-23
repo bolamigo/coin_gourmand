158c224f9fdbb7052dd909a24e9d213be036c54d
bolamigo <mendynicol@cy-tech.fr>
2023-05-23 20:03:56 +0200

    Search bar fixed
    Signed-off-by: bolamigo <mendynicol@cy-tech.fr>

--- search_bar.php
+++ search_bar.php
@@ -1,13 +1,22 @@
 <?php
-include "sql.php";
-@$keywords = $_GET["keywords"]; // récupérer les mots clés
-// TODO check $keywords to avoid SQL injection
-$res = $conn->prepare(
-    "SELECT title FROM recipe WHERE title LIKE '%$keywords%'"
-);
-$res->setFetchMode(PDO::FETCH_ASSOC);
-$res->execute();
-$tab = $res->fetchAll();
+include "sql.php"; // connect to the database
+$keywords = $_GET["keywords"]; // retrieve keywords
+$valid = preg_match('/^[a-zA-Z0-9 àâçéèêëîïôùûüÀÂÇÉÈÊËÎÏÔÙÛÜ\"\'\-]+$/', $keywords); // ensure the user input is coherent and avoid sql injection
+function strtosqlregex($string){
+    $search = array(' ', '\'', 'À', 'Â', 'Ç', 'É', 'È', 'Ê', 'Ë', 'Î', 'Ï', 'Ô', 'Ù', 'Û', 'Ü');
+    $replace = array('%', '\'\'', 'à', 'â', 'ç', 'é', 'è', 'ê', 'ë', 'î', 'ï', 'ô', 'ù', 'û', 'ü');
+    return str_replace($search, $replace, strtolower($string));
+}
+if($valid) {
+    $keywords_regex = strtosqlregex($keywords); // format the keywords for sql regex
+    $res = $conn->prepare(
+        "SELECT title FROM recipe WHERE title LIKE '%$keywords_regex%';" // find corresponding recipes
+    );
+    $res->setFetchMode(PDO::FETCH_ASSOC);
+    $res->execute();
+    $tab = $res->fetchAll(); // put results in a table
+}
+else $tab = [];
 ?>
 
 <!DOCTYPE html>
@@ -23,7 +32,7 @@ $tab = $res->fetchAll();
 		<div class="container">
 			<h1 id="main_title">Résultats de recherche</h1>
 			<form action="search_bar.php" method="get">
-				<input type="text" name="keywords" value=<?php echo $keywords; ?> required>
+				<input type="text" name="keywords" value="<?php echo str_replace('"', '&quot;', $keywords); ?>" required>
 				<ion-icon name="search"></ion-icon>
 			</form>
 			<div id="search_results">
@@ -31,9 +40,13 @@ $tab = $res->fetchAll();
                 for ($i = 0; $i < count($tab); $i++) {
                     echo "<div class='item' id='".$tab[$i]["title"]."'></div>";
                 }
-                if (count($tab) == 0) {
-                    echo "<p id='no_result'>Aucun résultat.</p>";
-                }
+                    if(!$valid){
+                        echo "<p id='no_result'>Caractères invalides."."<br>".
+                        "Caractères autorisés : lettres, chiffres, espace, lettre accentuées françaises, tiret, apostrophe, guillemets.</p>";
+                    }
+                    else if (count($tab) == 0) {
+                        echo "<p id='no_result'>Aucun résultat.</p>";
+                    }
                 ?>
             </div>
 	</section>
