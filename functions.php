b357f3d36648e11e8ba338dc86294b6170abbcfb
bolamigo <mendynicol@cy-tech.fr>
2023-05-30 19:32:47 +0200

    minor : function comments

--- functions.php
+++ functions.php
@@ -1,14 +1,14 @@
 <?php
 include "sql.php"; // Connection to the database.
 
-// This function formats the string to be displayed as French text and read by French users
+// This function formats the string to be displayed as French text and read by French users.
 function format_fr($string) {
-    $string = str_replace('_', ' ', $string); // Replace all underscores (_) with spaces (' ')
-    $string = str_replace('%27', "'", $string); // Replace all url-formatted single quotes (%27) to single quotes (')
-    $string = ucfirst($string); // Capitalize the first character
-    return $string; // Return the formatted string
+    $string = str_replace('_', ' ', $string); // Replace all underscores (_) with spaces (' ').
+    $string = ucfirst($string); // Capitalize the first character.
+    return $string; // Return the formatted string.
 }
 
+// This function searches for a given recipe in the database and returns the first result in a fields tab.
 function search_recipe($db, $query) {
 	// Get the recipe info, take the first result. Should be only 1.
 	var_dump($query);
