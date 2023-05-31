864bcc4c4b97c2ab12e8a59a5036e5c28b8d3bb8
bolamigo <mendynicol@cy-tech.fr>
2023-05-31 16:33:49 +0200

    fix : image links

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 		const author = $(this).attr('data-author');//TODO show author name
 
 		// auto-generate img/title/link with id
-		$(this).append(`<img src="image/${id}.jpg">`);
+		$(this).append(`<img src="recipe/image/${id}.jpg">`);
 		$(this).append(`<div class='flex-container'><h2>${format_fr(title)}</h2></div>`);
 		$(this).prop('title', format_fr(title));
 		$(this).click(function() { // When user clicks on the recipe (either image or title)
