0b9c22f459f3d64d5a56daf8d8ee15783ec12796
bolamigo <mendynicol@cy-tech.fr>
2023-06-01 04:00:35 +0200

    big update : recipe info is read from XML and displayed
    Signed-off-by: bolamigo <mendynicol@cy-tech.fr>

--- functions.php
+++ functions.php
@@ -8,17 +8,62 @@ function format_fr($string) {
     return $string; // Return the formatted string.
 }
 
-// This function searches for a given id_recipe in the database and returns the unique result .
-function search_recipe($db, $query) {
-	$res = $db->prepare(
-        "SELECT * FROM recipe WHERE id = :query ;"
+// This function searches for a given recipe ID in the database and returns the unique result.
+function search_recipe($id) {
+    global $conn;
+	$res = $conn->prepare(
+        "SELECT * FROM recipe WHERE id = :query;"
     );
-    $res->bindParam(':query', $query);
+    $res->bindParam(':query', $id);
 	$res->setFetchMode(PDO::FETCH_ASSOC);
 	$res->execute();
-	$tab = $res->fetchAll(); // Put all the recipe info in a table.
+	$tab = $res->fetchAll(); // Put the recipe into in a table.
+	return $tab[0]; // Return the first and only element of the table, the recipe.
+}
+
+// This function searches for one or more given ingredient ID in the database and returns the results in a tab.
+function search_ingredients($IDs) {
+    global $conn;
+    $IDs = implode(',', array_keys($IDs));
+	$res = $conn->prepare(
+        "SELECT `name`, `unit`, `media_id` FROM ingredient WHERE id in ($IDs) order by id;"
+    );
+	$res->setFetchMode(PDO::FETCH_ASSOC);
+	$res->execute();
+	$tab = $res->fetchAll(); // Put all the ingredients into in a table.
 	return $tab;
 }
+
+// This function searches for one or more given ustensil ID in the database and returns the results in a tab.
+function search_ustensils($IDs) {
+    global $conn;
+    $IDs = implode(',', $IDs);
+	$res = $conn->prepare(
+        "SELECT `name`, `media_id` FROM ustensil WHERE id in ($IDs);"
+    );
+	$res->setFetchMode(PDO::FETCH_ASSOC);
+	$res->execute();
+	$tab = $res->fetchAll(); // Put all the ingredients into in a table.
+	return $tab;
+}
+
+// This function gets the URL of a media object from its ID.
+function get_media($id) {
+    global $conn;
+	$res = $conn->prepare(
+        "SELECT `url` FROM media WHERE id = :query;"
+    );
+    $res->bindParam(':query', $id);
+	$res->setFetchMode(PDO::FETCH_ASSOC);
+	$res->execute();
+	$tab = $res->fetchAll();
+	return $tab[0];
+}
+
+// Utility function for debug, displays the content of a variable
+function echovar($var) {
+    echo "<pre>"; // <pre> tag is used to display tables with multiple lines
+    print_r($var);
+    echo "</pre>";
+}
 ?>
-
-
--- recipe.php
+++ recipe.php
@@ -1,24 +1,59 @@
 <?php
-header('Content-type: text/html; charset=UTF-8');
-include "functions.php"; // Connect to the database.
+include "functions.php";
 
 if (isset($_GET['r_id'])){
 	$id = $_GET['r_id'];
 	// Ensure the user input is coherent ('_' and no space) and avoid sql injection.
-	$valid = preg_match('/^[\w\pL\pMàâçéèêëîïôùûüÀÂÇÉÈÊËÎÏÔÙÛÜ\"\'\-]+$/', $id);
-	if($valid) {
-		$recipe = search_recipe($conn, $id);
-		if(count($recipe) == 0) {
-			$valid = false; // Invalid if there is no result.
-		}
+	$valid_query = preg_match('/^[\w\pL\pMàâçéèêëîïôùûüÀÂÇÉÈÊËÎÏÔÙÛÜ\"\'\-]+$/', $id);
+	if($valid_query) {
+		$recipe_db = search_recipe($id);
+		if(count($recipe_db) == 0)
+			$valid_query = false; // Invalid if there is no result.
 	}
 }
-if(!$valid or !isset($_GET['r_id'])) { // Not "else" because valid can be set false in the if block.
-	$recipe = search_recipe($conn, '4'); // display Apple Pie by default.
+
+if(!$valid_query || !isset($_GET['r_id'])) {
+	$id = 4;
+	$recipe_db = search_recipe($id); // display Apple Pie by default.
 }
 
-$recipe = $recipe[0]; // 1 result, simplify syntax.
-$title = format_fr($recipe["title"]);
+$recipe_xml = XMLReader::open("recipe/$id.xml");
+if(!$recipe_xml){ // File not found
+	$id = 4;
+	$recipe_db = search_recipe($id); // display Apple Pie by default.
+	$recipe_xml = XMLReader::open("recipe/$id.xml");
+}
+
+$title = format_fr($recipe_db["title"]);
+$ingredients = array();
+$ustensils = array();
+$steps = array();
+
+while($recipe_xml->read()) { // Go through the XML tree
+
+	// Filter only opening tags
+	if($recipe_xml->nodeType !== 1)
+		continue;
+
+	// Filter by tag name
+	switch($recipe_xml->name) {
+		case 'id': // Ingredient IDs
+			$ingredient = $recipe_xml->readString();
+
+			// Fetch the corresponding amount
+			while($recipe_xml->name !== 'amount')
+				$recipe_xml->next();
+
+			$ingredients[$ingredient] = $recipe_xml->readString();
+
+			break;
+		case 'ustensil': // Ustensil IDs
+			array_push($ustensils, $recipe_xml->readString());
+			break;
+		case 'step': // The recipe steps
+			array_push($steps, $recipe_xml->readString());
+	}
+}
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
@@ -35,7 +70,38 @@ $title = format_fr($recipe["title"]);
 	<section>
 		<div class="container">
 			<h1 id="main_title"><?php echo $title?></h1> <div id="search_button"><ion-icon class="clickable" name="search"></ion-icon></div>
-			<h2>Recette</h2>
+			<h2>Ingrédients</h2>
+			<?php
+				ksort($ingredients);
+				$ingredients_db = search_ingredients($ingredients);
+				$ingredients_values = array_values($ingredients);
+				foreach ($ingredients_db as $ingredient) {
+					$name = $ingredient['name'];
+					$value = current($ingredients_values);
+					$unit = $ingredient['unit'] ?? '';
+					$image = get_media($ingredient['media_id']) ?? '';
+
+					echo "<div class='ingredient'>$name : $value $unit</div>"; // TODO formatting + image
+
+					next($ingredients_values);
+				}
+			?>
+			<h2>Ustensiles</h2>
+			<?php
+				$ustensils = search_ustensils($ustensils);
+				foreach ($ustensils as $ustensil) {
+					$name = $ustensil['name'];
+					$image = get_media($ustensil['media_id']) ?? '';
+
+					echo "<div class='ustensil'>$name</div>"; // TODO formatting + image
+				}
+			?>
+			<h2>Préparation</h2>
+			<?php
+				foreach ($steps as $step) {
+					echo "<div class='step'>$step</div>"; // TODO formatting
+				}
+			?>
 		</div>
 	</section>
 
