0886142e33e1f44e196a7ecb6b4307a329add28f
bolamigo <mendynicol@cy-tech.fr>
2023-06-05 23:00:12 +0200

    minor fix : recipe : close search button works
    Signed-off-by: bolamigo <mendynicol@cy-tech.fr>

--- recipe/recipe.js
+++ recipe/recipe.js
@@ -15,25 +15,27 @@ $(document).ready(async function() {
     // Adjusting the size of the parchment
     $("#parchment").css('height', $("#steps").height() - 64);
     $("#parchment").css('width', $("#steps").width() - 64);
+
 });
 
 function close_search_button() {
     $("#search_button").html("<ion-icon class='clickable' name='search'></ion-icon>");
+    $("#close_search").remove();
+    $("#search_button").css("top", "32px");
+    $("#search_button").css("right", "32px");
     $("#search_button").one("click", function () {
         $(this).html(
             "<form action='search_results.php' method='get'>"+
                 "<input type='text' name='keywords' placeholder='Rechercher une recette...' required>"+
                 "<ion-icon name='search'></ion-icon>"+
-            "</form>"+
-            "<ion-icon id='close_search' class='clickable' name='close-circle-outline'></ion-icon>"
+            "</form>"
         );
+        $('body').append("<ion-icon id='close_search' class='clickable' name='close-circle-outline'></ion-icon>");
         $(this).css("top", "0");
         $(this).css("right", "0");
         $("input:text:visible:first").focus();
-    });
-
-    // TODO marche pas ?
-    $("#close_search").on("click", function () {
-        close_search_button();
+        $("#close_search").one("click", function () {
+            close_search_button();
+        });
     });
 }
